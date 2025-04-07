d0716b510e56a2fa7c85dbd1fa07fa78832410a8
cerbaro-r <cerbarorafel1@gmail.com>
2025-04-07 14:33:16 -0300

    idade

---
+++ TPE 1 (lista 2)/4_idade.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main(){
+
+    int i;
+
+    printf("informe quando anos voce tem: ");
+    scanf("%d", &i);
+
+    if(i>=18){
+        printf("Maior de idade");
+    }else{
+        printf("Menor de idade");
+    }
+
+}
