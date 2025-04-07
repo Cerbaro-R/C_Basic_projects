f6158f4b29c9cb184efe481332b3592da6d2f8de
cerbaro-r <cerbarorafel1@gmail.com>
2025-04-07 08:51:46 -0300

    financiamento

---
+++ TPE 1 (lista 2)/2_Financiamento.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    float s, mf, f;
+
+    printf("Digite o seu salario: ");
+scanf("%f", &s);
+
+printf("Digite o valor que voce deseja financiar: ");
+scanf("%f", &f);
+
+mf = s*5;
+
+if(f<=mf){
+    printf("Financiamento concedido\n");
+}else{
+    printf("Financiamento negado\n");
+}
+
+printf("Obrigado por nos consultar");
+
+}
+
