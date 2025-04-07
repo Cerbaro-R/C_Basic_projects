51cfb5350772225a1bfb41cf3eca71543f15d9ff
Cerbaro-R <cerbarorafael1@gmail.com>
2025-04-06 21:27:46 -0300

    algorítimo para calcular bonus no salário de acordo com a quantidade de anos trbalhados

---
+++ Lista 1/11_bonus_salario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    float s, b;
+    char name[50];
+
+    printf("Digite seu nome: ");
+    scanf("%s", name);
+
+    printf("Digite seu salario em reais: ");
+    scanf("%f", &s);
+
+    printf("Digite a quantos anos voce trabalha na empresa: ");
+    scanf("%d", &a);
+
+    if(a<3){
+        b = s*0.05;
+        s = s+b;
+    }else if((a>2) && (a<6)){
+        b = s*0.1;
+        s = s+b;
+    }else if(a>5){
+        b = s*0.15;
+        s = s + b;
+    }
+
+    printf("Parabens %s, seu bonus e de %.2f então seu salário será de %.2f", name, b, s);
+}
