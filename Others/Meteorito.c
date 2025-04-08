e07019200acc0e6f55511f6517141e27d944da52
Cerbaro-R <cerbarorafael1@gmail.com>
2025-04-07 21:17:59 -0300

    meteorito

---
+++ Others/Meteorito.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+
+    int x1, y1, x2, y2, mx, my, m;
+    char s;
+
+    printf("Informe a coordenada x1:");
+    scanf("%d", &x1);
+    printf("Informe a coordenada y1:");
+    scanf("%d", &y1);    
+    printf("Informe a coordenada x2:");
+    scanf("%d", &x2);
+    printf("Informe a coordenada y2:");
+    scanf("%d", &y2);
+
+    printf("O valor das coordenadas sao %d, %d, %d, %d\n", x1, y1, x2, y2);
+
+    m = 0;
+    s = 's';
+
+    while (s == 's')
+    {
+        printf("informe a coordenada x do meteorito: ");
+        scanf("%d", &mx);
+        printf("informe a coordenada y do meteorito: ");
+        scanf("%d", &my);
+
+        if((mx>=x1) && (mx<=x2) && (my>=y1) && (my<=y2)){
+            printf("O meteorito caiu dentro da fazenda\n");
+            m= m + 1;
+        }else{
+            printf("O meteorito nao caiu dentro da fazenda\n");
+        }
+
+        printf("Deseja informar outra coordenada? (s/n)");
+        scanf(" %c", &s);
+    }
+    
+    printf("O numero de meteoritos que caiu na fazenda foi de: %d", m);
+}
