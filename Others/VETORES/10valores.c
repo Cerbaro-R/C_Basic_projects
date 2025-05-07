727d920cb4db0a8900d839fdbff84bfd1cd8fcd3
Cerbaro-R <cerbarorafael1@gmail.com>
2025-05-07 20:17:41 -0300

    Guardar posição do vetor maior menor

--- Others/VETORES/10valores.c
+++ Others/VETORES/10valores.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-    int n[10], i, maior=0, menor=99999999999999;
+    int n[10], i, maior=0, menor=99999999999999, pb, ps;
 
     for (i = 0; i < 10; i++)
     {
@@ -10,12 +10,14 @@ int main(){
         scanf("%d", &n[i]);
         if(n[i]>maior){
             maior = n[i];
+            pb = i+1;
         }if(n[i]<menor){
             menor = n[i];
+            ps = i+1;
         }
     }
-    printf("O maior valor e: %d \n", maior);
-    printf("O menor valor e: %d \n", menor);
+    printf("O maior valor e: %d e ocupa a %d posicao no vetor \n", maior, pb);
+    printf("O menor valor e: %d e ocupa a %d posicao no vetor \n", menor, ps);
     printf("Todos os valores sao: \n");
     for(i = 0; i<10; i++){
         printf("%d   ", n[i]);
