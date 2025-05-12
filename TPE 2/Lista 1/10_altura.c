e8852396edb752d28e6a01aec42e90a6c5c54e06
Cerbaro-R <cerbarorafael1@gmail.com>
2025-05-11 21:43:57 -0300

    altura

---
+++ TPE 2/Lista 1/10_altura.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int altura[15], baixa=0, alta=0;
+
+    for(int i=0; i<15; i++) {
+        printf("Informe a altura da %do pessoa em centimetros: ", i+1);
+        scanf("%d", &altura[i]);
+
+        if(altura[i] < 160) {
+            baixa++;
+        }else if(altura[i] > 180) {
+            alta++;
+        }
+    }
+
+    printf("\n%d pessoas tem menos de 160 cm\n", baixa);
+    printf("%d pessoas tem mais de 180 cm\n", alta);
+}
