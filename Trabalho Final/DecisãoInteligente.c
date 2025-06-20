dc3447b9a07febf80651e19833ab42c3fc989180
unknown <cerbarorafael1@gmail.com>
2025-06-19 21:50:37 -0300

    Relatório por horário

--- Trabalho Final/DecisãoInteligente.c
+++ Trabalho Final/DecisãoInteligente.c
@@ -15,12 +15,6 @@ void solicitaHr(int hora[], int minuto[], int *qtdHr){
     }
 }
 
-void imprimeHr(int hora[], int minuto[], int *qtdHr){
-    for(int i=0; i<*qtdHr; i++){
-        printf("%02d:%02d\n", hora[i], minuto[i]);
-    }
-}
-
 void solicitaQtdP(int *qtdP){
     printf("Informe quantas pessoas parcitiparao da reuniao: ");
     scanf("%d", qtdP);
@@ -40,14 +34,34 @@ void solicitaPessoas(struct Pessoa pessoas[], int *qtdP, int hora[], int minuto[
     }
 }
 
+void imprimeRelatorio(struct Pessoa pessoas[], int *qtdP, int *qtdHr, int hora[], int minuto[]){
+    int pessoasDisponiveis[50], qtdDisponivel=0;
+    printf("\n\n--------------Relatorio de disponibilidade--------------\n\n");    
+    for(int i=0; i<*qtdHr; i++){
+        qtdDisponivel = 0;
+        printf("Horario:    %d:%d \n", hora[i], minuto[i]);
+        for(int j =0; j<*qtdP; j++){
+            if(pessoas[j].disponibilidade[i]==1){
+                pessoasDisponiveis[qtdDisponivel] = j;
+                qtdDisponivel++;
+            }
+        }
+        printf("Pessoas disponiveis: ");
+        for(int k=0; k<qtdDisponivel; k++){
+            printf("\n- %s", pessoas[pessoasDisponiveis[k]].nome);
+        }
+        printf("\n---------------------------------\n\n");
+    }
+}
+
 int main(){
 
     int hora[50], minuto[50], qtdHr, qtdP;
 
     solicitaHr(hora, minuto, &qtdHr);
-    imprimeHr(hora, minuto, &qtdHr);
     solicitaQtdP(&qtdP);
     struct Pessoa pessoas[qtdP];
     solicitaPessoas(pessoas, &qtdP, hora, minuto, &qtdHr);
+    imprimeRelatorio(pessoas, &qtdP, &qtdHr, hora, minuto);
 
 }
